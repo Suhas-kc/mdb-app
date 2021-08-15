b421e72434ae45a94f640143f116f0229fc8680e
Suhas <suhasksl@gmail.com>
2021-08-16 00:08:42 +0530

    Initial commit with functional components

---
+++ src/components/MovieInfoBar/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import { calcTime,convertMoney} from '../../helpers';
+import PropTypes from 'prop-types';
+import { Wrapper,Content } from './MovieInfoBar.styles';
+
+export const MovieInfoBar = ({time,budget,revenue}) => (
+    <Wrapper>
+        <Content>
+            <div className="column">
+                <p>Running time: {calcTime(time)}</p>
+            </div>
+            <div className="column">
+                <p>Budget: {convertMoney(budget)}</p>
+            </div>
+            <div className="column">
+                <p>Revenue: {convertMoney(revenue)}</p>
+            </div>
+        </Content>
+    </Wrapper>
+);
+
+MovieInfoBar.propTypes = {
+    time: PropTypes.number,
+    budget: PropTypes.number,
+    revenue: PropTypes.number
+}
